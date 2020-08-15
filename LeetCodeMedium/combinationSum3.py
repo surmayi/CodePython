0706b139d882171fc8879045d9b15d331ad1f0f1
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 18:12:51 +0530

    Update combinationSum3.py

--- LeetCodeMedium/combinationSum3.py
+++ LeetCodeMedium/combinationSum3.py
@@ -1,16 +1,15 @@
-class Solution:
-    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+class Solution(object):
+    def combinationSum3(self, k, n):
+        nums = range(1,10)
         res=[]
-        candidates=range(1,10)
-        self.combine_helper(candidates,k, n,[],res)
+        self.combine_helper(nums,k,n,[],res)
         return res
-
-    def combine_helper(self,candidates,length, target,path,res):
+    def combine_helper(self,nums,length,target,path,res):
         if length==0 and target==0:
-            if path not in res:
-                res.append(path)
-                return
-        for i in range(len(candidates)):
-            if target-candidates[i]<0 or length<0:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            if target-nums[i] <0 or length <0:
                 break
-            self.combine_helper(candidates[i+1:],length-1,target-candidates[i],path+[candidates[i]],res)
+            self.combine_helper(nums[i+1:],length-1,target-nums[i],path+[nums[i]],res)
+        
