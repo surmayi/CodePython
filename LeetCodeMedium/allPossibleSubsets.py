20bf3b2941dd79d81850acc1aa426085cf3dc7c1
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 16:30:16 +0530

    Update allPossibleSubsets.py

--- LeetCodeMedium/allPossibleSubsets.py
+++ LeetCodeMedium/allPossibleSubsets.py
@@ -1,3 +1,4 @@
+# Iteration
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = [[]]
@@ -5,3 +6,17 @@ class Solution:
             for i in result:
                 result = result + [i + [num] ]
         return result
+ 
+# Recursion
+class Solution(object):
+    def subsets(self, nums):
+        res =[]
+        self.set_helper(nums,0,[],res)
+        return res
+
+    def set_helper(self,nums,index,path,res):
+        res.append(path)
+        for i in range(index,len(nums)):
+            self.set_helper(nums,i+1,path+[nums[i]],res)
+        return res
+        
