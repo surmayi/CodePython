b093d7351a7eeac0442354875f34e90ebc21c215
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 20:09:28 +0530

    Update checkingMissingFromSeries.py

--- LeetCodeEasy/checkingMissingFromSeries.py
+++ LeetCodeEasy/checkingMissingFromSeries.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def missingNumber(self, nums):
-        n= len(nums)
-        sum1 = n*(n+1)/2
-        for i in nums:
-            sum1=sum1-i
-        return sum1
+        return (len(nums)*(len(nums)+1)/2)- sum(nums)
+        
