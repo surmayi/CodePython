e88798125000503c759b4015ddd8ace4c232f065
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 13:59:41 +0530

    Update findSingleDuplicateLinkList.py

--- LeetCodeMedium/findSingleDuplicateLinkList.py
+++ LeetCodeMedium/findSingleDuplicateLinkList.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def findDuplicate(self, nums):
         slow, fast = nums[0], nums[0]
-        #first find the index from where to start searching
+        #first find the index from where to start searching, slow jumps 1 step, fast jumps 2 steps, until they meet, ie both have same value
         while True:
             slow, fast = nums[slow], nums[nums[fast]]
             if slow == fast: break
