22d230d790e105ed829c6eb7b90f20f76106d0ff
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 13:35:15 +0530

    Update generateParenthesis.py

--- LeetCodeMedium/generateParenthesis.py
+++ LeetCodeMedium/generateParenthesis.py
@@ -1,14 +1,15 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        self.dfs(n, n, "", res)
+class Solution(object):
+    def generateParenthesis(self, n):
+        res=[]
+        self.param_helper(n,n,'',res)
         return res
 
-    def dfs(self, leftRemain, rightRemain, path, res):
-        if leftRemain > rightRemain or leftRemain < 0 or rightRemain < 0:
-            return  # backtracking
-        if leftRemain == 0 and rightRemain == 0:
+    def param_helper(self,leftRemain,rightRemain,path,res):
+        if leftRemain>rightRemain or leftRemain<0 or rightRemain<0:
+            return
+        if leftRemain ==0 and rightRemain==0:
             res.append(path)
-            return 
-        self.dfs(leftRemain-1, rightRemain, path+"(", res)
-        self.dfs(leftRemain, rightRemain-1, path+")", res)
+            return res
+        self.param_helper(leftRemain-1,rightRemain,path+'(',res)
+        self.param_helper(leftRemain,rightRemain-1,path+')',res)
+        
