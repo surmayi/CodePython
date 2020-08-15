ff8110ca1e1274a82d6c552cc2aa85df61a2d5ea
Surmayi Shree <surmayi7@gmail.com>
2020-08-15 20:05:40 +0530

    Update containsDuplicate.py

--- LeetCodeEasy/containsDuplicate.py
+++ LeetCodeEasy/containsDuplicate.py
@@ -1,10 +1,4 @@
-def containsDuplicate( nums):
-        ol= len(nums)
-        nl= len(set(nums))
-        if ol==nl:
-            return False
-        return True
-
-
-containsDuplicate([1,2,3,1])
-containsDuplicate([1,2,3,4])
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(nums) > len(set(nums))
+        
