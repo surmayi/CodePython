3f9b74e8079342012eca53f8dfb2006cbd7219ce
Surmayi Shree <surmayi7@gmail.com>
2021-10-11 16:22:12 -0500

    Delete PythonLearning/Algorithms directory

--- PythonLearning/Algorithms/Patterns/TriangleStarPattern.py
+++
@@ -1,5 +0,0 @@
-def printStar(size):
-    for i in range(size):
-        print(' '*(size-i-1)+'*'*(2*i+1))
-
-printStar(5)
--- PythonLearning/Algorithms/Patterns/starPlus.py
+++
@@ -1,8 +0,0 @@
-a=''
-for i in range(5):
-    if i%2==1:
-        a+='+'
-    else:
-        a+='*'
-    print(a)
-
